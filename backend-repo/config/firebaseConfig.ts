8b90fd635a0a03d688010935f62855360977302a
hunandika <hunandikaadyota@gmail.com>
2025-02-16 02:04:16 +0700

    refactor firebase function can run on root

--- backend-repo/config/firebaseConfig.ts
+++ backend-repo/config/firebaseConfig.ts
@@ -1,17 +1,17 @@
 import admin from 'firebase-admin'
 
 const firebaseConfig = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    type: process.env.FIRE_TYPE,
+    project_id: process.env.FIRE_PROJECT_ID,
+    private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
+    private_key: process.env.FIRE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIRE_EMAIL,
+    client_id: process.env.FIRE_CLIENT_ID,
+    auth_uri: process.env.FIRE_AUTH_URI,
+    token_uri: process.env.FIRE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIRE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIRE_UNIVERSE_DOMAIN,
 }
 
 admin.initializeApp({
--- backend-repo/core/index.ts
+++ backend-repo/core/index.ts
@@ -1,8 +1,9 @@
 import logger from '#/utils/logger'
 import app from './app'
-const PORT = process.env.PORT || 3000
+
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 3000
 
 // Start the server
-app.listen(PORT, () => {
-    logger.info(`Server is running on http://localhost:${PORT}`)
+app.listen(EXPRESS_PORT, () => {
+    logger.info(`Server is running on http://localhost:${EXPRESS_PORT}`)
 })
